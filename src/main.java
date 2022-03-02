8b51e67268322d57c04ed0265dc3bc9f2948f1f1
Salvador Sobrino <salvador.sobrino.solorzano.1p@gmail.com>
2022-03-02 15:44:48 +0100

    pruebas de suma en el main

--- src/main.java
+++ src/main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce el primer número: ");
+		int n1 = sc.nextInt();
+		System.out.println("Introduce el segundo número: ");
+		int n2 = sc.nextInt();
+		System.out.println("La suma resultante es: " + Suma.sumarDosNumeros(n1, n2));
+		System.out.println("Introduce otro número: ");
+		int n = sc.nextInt();
+		System.out.println("La suma acumulada es: " + Suma.sumaAcumulada(n));
 
 	}
 
