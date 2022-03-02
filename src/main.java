6c5028523cd216e5630e5626aac59a70f61b9b69
Salvador Sobrino <salvador.sobrino.solorzano.1p@gmail.com>
2022-03-02 17:24:05 +0100

    prueba main de la clase resta en la rama resta

--- src/main.java
+++ src/main.java
@@ -13,7 +13,18 @@ public class main {
 		System.out.println("Introduce otro número: ");
 		int n = sc.nextInt();
 		System.out.println("La suma acumulada es: " + Suma.sumaAcumulada(n));
-
+		
+		System.out.println("Introduce el primer número: ");
+		int num1 = sc.nextInt();
+		System.out.println("Introduce el segundo número: ");
+		int num2 = sc.nextInt();
+		System.out.println("La resta resultante es: " + Resta.restarDosNumeros(num1, num2));
+		System.out.println("Introduce otro número: ");
+		int num = sc.nextInt();
+		System.out.println("La resta acumulada es: " + Resta.restaAcumulada(num));
+		
+		
+		//pruebas de la clase resta hacer commit y add
 	}
 
 }
